386613b7edd2ca29a9f61953ca341592e6954722
m-kojima <5977111+m-kojima@users.noreply.github.com>
2017-11-20 01:24:58 +0900

    transition sample

--- app/src/main/java/com/example/fragmenttransition/MainActivity.java
+++ app/src/main/java/com/example/fragmenttransition/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.fragmenttransition;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.fragmenttransition.fragments.FirstFragment;
+import com.example.fragmenttransition.fragments.SecondFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.first:
+                transition(FirstFragment.newInstance("", ""));
+                break;
+            case R.id.second:
+                transition(SecondFragment.newInstance("", ""));
+                break;
+        }
+        return true;
+    }
+
+    public void transition(Fragment fragment) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+//        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+//        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        transaction.setCustomAnimations(
+                R.anim.transition_push_enter, R.anim.transition_push_exit,
+                R.anim.transition_pop_enter, R.anim.transition_pop_exit);
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
